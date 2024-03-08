ae71f0ef1873eea9eaa22c74f8aa0f3ae80e6775
gabEKMorais <gabekmorais@gmail.com>
2024-03-08 13:26:01 -0300

    Segunda Commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "bb23477a831f08c3"; }
+/******/ 		__webpack_require__.h = function() { return "0d3d96a09b72bd32"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
