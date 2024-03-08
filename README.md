48b005ba881fe0bf271138fd5347859ebffd9a50
gabEKMorais <gabekmorais@gmail.com>
2024-03-08 13:11:26 -0300

    Primeira Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Projeto_Web
+Desenvolvimento WEB com next.js
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home () {
+    return <div><h1>22:30 https://www.youtube.com/watch?v=EW7m2WIvFgQ</h1></div>
+}
+
+export default Home
